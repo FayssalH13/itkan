3c322eceec153f68bff93d9949bc2cf2e65fb1b6
Fayssal Hawari <faysalhawari13@gmail.com>
2025-08-07 15:39:13 +0300

    adding routes files for admin side

---
+++ src/user/pages/UserHome.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function UserHome() {
+  return (
+    <div>
+      <h1>User Homepage</h1>
+      <p>Welcome to the user-facing side of the app.</p>
+    </div>
+  );
+}
